a7d10d019ecbdbe1a167cf312676f5cdf7eb27fe
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-10-10 23:57:24 +0200

    ajout de javascript

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+/* Création d'une modale pour expliquer le jeu 
+
 const start = document.getElementById("start");
-start.onclick = () => alert("Patience, le jeu arrive bientôt !");
+
+start.onclick = () => alert("Patience, le jeu arrive bientôt !");*/
+
+
+//Animation prénom
+const button = document.getElementById("start");
+button.addEventListener("click", () => {
+    const name = prompt("Entrez votre prénom");
+        if (name !== null && name.trim() !== "") {
+            if (/^[A-Za-z]+$/.test(name)) {
+                button.textContent = `Bienvenue ${name} et bonne visite à toi !`;
+            } else {
+                alert("Règle du jeu numéro 1 : ne saisir que des lettres ;-\)");
+            }
+        } else {
+            alert("Règle du jeu numéro 2 : saisir des lettres ;-\)");
+        }
+});
+
+$(document).ready(function() {
+    // On charge les données de la section "Actualités"
+    $.ajax({
+      url: "Index.html",
+      method: "GET",
+      success: function(data) {
+        // On affiche les données dans la section "Actualités"
+        $("#Index").html(data);
+      }
+    });
+  });
+
 
